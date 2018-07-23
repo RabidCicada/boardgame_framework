ae5b2041eb183f8ff619f611f3232a0155c47b35
Kyle Stapp <rabidcicada@gmail.com>
2018-07-23 01:24:21 -0400

    Initial Stab at sphinx, and pbr, and packaging.

---
+++ dev/install_and_configure_pyenv.sh
@@ -0,0 +1,18 @@
+#Install pyenv and configure 3.6/3.7
+curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer -o pyenv-installer.sh
+chmod 777 pyenv-installer.sh
+./pyenv-installer.sh
+cat >> ~/.bash_profile <<'ZZZBBB'
+#Specifically a fix for Ubuntu on Windows
+if [ -f ~/.bashrc ] ; then
+        . ~/.bashrc
+fi
+
+export PATH="~/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+ZZZBBB
+source ~/.bash_profile
+pyenv install 3.6.4
+pyenv install 3.7.0
+pyenv virtualenv 3.7.0 bgfw_dev
---
+++ src/bgfw/actor.py
@@ -0,0 +1,12 @@
+
+
+class Actor():
+    """
+    Be the common interface for agents that interact with the game.
+    This can be a player, AI, Dungeon Master, etc.
+
+    The Actor will control pawns, and possibly other game items in the game state.
+    """
+
+    def act():
+        pass
