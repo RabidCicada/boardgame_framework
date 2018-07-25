3139cc7f292df7944bc081c4c5b77b5bb30c782d
Kyle Stapp <rabidcicada@gmail.com>
2018-07-25 19:16:56 -0400

    forgot one line in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 
 setup(
     setup_requires=['pbr'],
-    #package_dir={'':'src/'}, # Needed for distutils
+    package_dir={'':'src/'}, # Needed for distutils
     pbr=True
 )
