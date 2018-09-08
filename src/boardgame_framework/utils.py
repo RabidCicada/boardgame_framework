435cf90f9dcdd7a81141ad5d8ab24caab3d3f632
Kyle T Stapp <RabidCicada@gmail.com>
2018-09-07 23:02:15 -0400

    More dev.  Not operational yet

--- src/boardgame_framework/utils.py
+++ src/boardgame_framework/utils.py
@@ -1,45 +1,180 @@
 import pathlib
-from boardgame_framework.cell import Cell
+from . import cell
+from enum import Enum
+from dataclasses import dataclass
 
-class CellManager():
-    cell_parsers = {}
+
+class CoordinateSystemMgr():
+    coord_systems = dict()
 
     #The following uses decorator syntax to perform processing
     #on the function but doesn't actually wrap it.
     @classmethod
-    def register_cell_parser(cls,extension):
+    def register_coord_system(cls,system_name):
+        def anon_reg_func(coord_system):
+            if not issubclass(coord_system,CoordinateSystem):
+                raise ValueError(F"Passed class {coord_system.__name__} not of type CoordinateSystem")
+            coord_system.shortname = system_name
+
+            #register both the short name and the full class name
+            cls.coord_systems[system_name] = coord_system
+            cls.coord_systems[coord_system.__name__] = coord_system
+            return coord_system
+        return anon_reg_func
+
+    @classmethod
+    def get_coord_system(cls,system_name):
+        if system_name in cls.coord_systems:
+            return cls.coord_systems[system_name]()
+
+    @classmethod
+    def get_coord_type(cls,system_name):
+        if system_name in cls.coord_systems:
+            return cls.coord_systems[system_name]
+
+
+class CoordinateSystem():
+    to_other_conversions={}
+    from_other_conversions={}
+
+    def __str__(self):
+        return F"{type(self).__name__}"
+
+    def __repr__(self):
+        return F"{type(self).__name__}-{id(self)}"
+
+    @classmethod
+    def register_to_converter(cls,coord_system):
         def anon_reg_func(callback):
-            cls.cell_parsers[extension] = callback
+            cls.to_other_conversions[coord_system] = callback
             return callback
         return anon_reg_func
 
     @classmethod
-    def load_cells(cls,cells_path):
-        cls.cell_parsers[pathlib.Path(cells_path).suffix](cells_path)
+    def register_from_converter(cls,coord_system):
+        def anon_reg_func(callback):
+            cls.from_other_conversions[coord_system] = callback
+            return callback
+        return anon_reg_func
 
     @classmethod
-    def create_cell(cell_dict):
-
-        #Passthrough all the initialization we can for simple construction
-        Cell(**cell_dict)
-
-        #auxiliary property parsing
-        cell.attributes.update(cell_dict.attributes)
-
-        for cell in cell_dict.cells:
-
-            #parse subcells
-            cls.create_cell(subcell_dict)
+    def to_other_system(cls,name,coord):
+        if name in to_other_conversions:
+            to_other_conversions[name](coord)
+        else:
+            raise ValueError(F"No Converter to {name} Found")
 
 
-@CellManager.register_cell_parser('.json')
-def load_cell_json(cell_path):
-    import json
-    with open(cell_path, "r") as json_cells:
-        data = json.load(json_cells)
+    @classmethod
+    def from_other_system(cls,name,coord):
+        if name in to_other_conversions:
+            to_other_conversions[name](coord)
+        else:
+            raise ValueError(F"No Converter from {name} Found")
 
-@CellManager.register_cell_parser('.yml')
-def load_cell_yml(cell_path):
-    import yaml
-    with open(cell_path, "r") as yaml_cells:
-        data = yaml.load(yaml_cells)
+
+@CoordinateSystemMgr.register_coord_system('cubed')
+class CubedCoordinateSystem(CoordinateSystem):
+    dimension_req_keys=['x','y','z']
+
+
+
+    #coord_type = cubed_coord
+
+    class FromRectOffsetType(Enum):
+        ODDR = 1
+        EVENR = 2
+        ODDC = 3
+        EVENC = 4
+
+    def __init__(self):
+        pass
+
+
+
+    def coord():
+        return cubed_coord(**kwargs)
+
+    def get_coord_set(self,dimensions):
+        #safety checking
+        if not all (key in dimensions for key in self.dimension_req_keys):
+            raise ValueError(f"Missing one of required keys {self.dimension_req_keys}")
+
+    def validate_dimensions(self,dimensions):
+        #safety checking
+        if not all (key in dimensions for key in self.dimension_req_keys):
+            raise ValueError(f"Missing one of required keys {self.dimension_req_keys}")
+
+        if not all(isinstance(elem) for elem in dimensions):
+            raise ValueError(f"Not all dimensions are ints {dimensions}")
+
+    def get_coord_set(self,dimensions):
+        assert False, "Not Implemented Yet"
+
+        return coords
+
+    def from_rect_coord(self,rect_coord,offset_type=FromRectOffsetType.ODDR):
+        assert(rect_coord.coord_type==RectCoordinateSystem)
+
+    @CoordinateSystem.register_from_converter("rect")
+    @staticmethod
+    def from_oddr(coord):
+        x = coord.x - (coord.y - (coord.y&1)) / 2
+        z = coord.y
+        y = -x-z
+        return cubed_coord(x, y, z)
+
+    @CoordinateSystem.register_to_converter("rect")
+    @staticmethod
+    def to_oddr(coord):
+        col = coord.x + (coord.z - (coord.z&1)) / 2
+        row = coord.z
+        return RectCoordinateSystem.rect_coord(col, row)
+
+
+@dataclass
+class cubed_coord():
+    x: int
+    y: int
+    z: int
+    system: CoordinateSystem = CubedCoordinateSystem
+
+@CoordinateSystemMgr.register_coord_system('rect')
+class RectCoordinateSystem(CoordinateSystem):
+    dimension_req_keys=['x','y']
+
+
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return F"{type(self).__name__}"
+
+    def __repr__(self):
+        return F"{type(self).__name__}-{id(self)}"
+
+    def validate_dimensions(self,dimensions):
+        #safety checking
+        if not all (key in dimensions for key in self.dimension_req_keys):
+            raise ValueError(F"Missing one of required keys {self.dimension_req_keys}")
+
+        if not all(isinstance(elem) for elem in dimensions):
+            raise ValueError(F"Not all dimensions are ints {dimensions}")
+
+    def get_coord_set(self,dimensions):
+        coords = set()
+
+        validate_dimensions(dimensions)
+
+        for x in range(0,dimensions['x']):
+            for y in range(0,dimensions['y']):
+                coords.add(rect_coord(x,y))
+
+        return coords
+
+@dataclass
+class rect_coord():
+    x: int
+    y: int
+    coord_type: CoordinateSystem = RectCoordinateSystem
--- tests/test_cell.py
+++ tests/test_cell.py
@@ -1,6 +1,18 @@
-import boardgame_framework.cell as cell
+from boardgame_framework.cell import Cell
 import boardgame_framework.utils as utils
 
-def test_cell():
-    CMgr = utils.CellManager()
-    CMgr.load_cells("./data_files/level1map.json")
+def test_explicit_cell_yaml():
+    cells = Cell.load_cells("../tests/data_files/level1_explicit_map.yml")
+
+    assert len(cells) == 9
+
+def test_auto_cell_yaml():
+    cells = Cell.load_cells("../tests/data_files/level1_auto_map.yml")
+
+    assert len(cells) == 48
+
+def test_explicit_cell_json():
+    cells = Cell.load_cells("../tests/data_files/level1_explicit_map.json")
+
+# def test_auto_cell_json():
+#     Cell.load_cells("tests/data_files/level1_auto_map.json")
