ef81f3f6f1a27994eaf7dea336a12149db4c601a
Kyle T Stapp <RabidCicada@gmail.com>
2018-09-21 15:36:16 -0400

    More updates.  Implemented coordinates and coordinate math.  Implemented ranging.  Auto cell generating from directives in map file.  Coordinate transformation from rectangular to hexagonal etc

--- tests/test_cell.py
+++ tests/test_cell.py
@@ -4,15 +4,18 @@ import boardgame_framework.utils as utils
 def test_explicit_cell_yaml():
     cells = Cell.load_cells("../tests/data_files/level1_explicit_map.yml")
 
-    assert len(cells) == 9
+    assert len(cells) == 11
 
 def test_auto_cell_yaml():
     cells = Cell.load_cells("../tests/data_files/level1_auto_map.yml")
 
-    assert len(cells) == 48
+    #4 top level cells + 48 + 42 + 19 + 35
+    assert len(cells) == 148
 
 def test_explicit_cell_json():
     cells = Cell.load_cells("../tests/data_files/level1_explicit_map.json")
+
+    assert len(cells)  == 11
 
 # def test_auto_cell_json():
 #     Cell.load_cells("tests/data_files/level1_auto_map.json")
