2685295ceb48dcdb211b26aa8c9ce25991d30eb9
Kyle Stapp <kstapp@redlattice.com>
2018-09-21 15:36:16 -0400

    More updates.  Implemented coordinates and coordinate math.  Implemented ranging.  Auto cell generating from directives in map file.  Coordinate transformation from rectangular to hexagonal etc

--- src/boardgame_framework/cell.py
+++ src/boardgame_framework/cell.py
@@ -4,8 +4,10 @@ import logging
 
 from .utils import CoordinateSystemMgr
 
-auto_cells_required_keys = ["cell_type","coord_type","dimmensions"]
-cell_type_coord_mapping = {"hex":"cubed","square":"rect","linear":"linear"}
+auto_cells_required_keys = ["cell_type","system","dimensions"]
+cell_type_coord_mapping = {"hex":"hex","square":"square","linear":"linear"}
+
+logger = logging.getLogger(__name__)
 
 class Cell():
     """
@@ -19,20 +21,20 @@ class Cell():
     new_uid = itertools.count()
     cell_parsers = dict()
 
-    def __init__(self, cell_type, uid=None, name=None, attributes=None, coord=None, **kwargs):
+    def __init__(self, cell_type=None, uid=None, name=None, attributes=None, coord=None, connections=None, **kwargs):
 
         self.name = name
-        self.uid = next(new_uid) if uid is None else uid
-        self.connections = set()
+        self.uid = next(self.new_uid) if uid is None else uid
+        self.connections = connections
         self.attributes = attributes
         self.cell_type = cell_type
-        self.coord_system = cell_type_coord_mapping[self.cell_type]
+        self.coord_system = CoordinateSystemMgr.get_coord_system(cell_type_coord_mapping[self.cell_type]) if cell_type else None
 
         if coord:
-            if isinstance(coord.system, CoordinateSystemMgr.get_coord_system(self.coord_system)):
+            if isinstance(coord.system, type(self.coord_system)):
                 self.coord = coord
             else:
-                self.coord = self.coord_system.from_other_system(coord.system.shortname,coord)
+                self.coord = self.coord_system.from_other_system(coord.system.system_name,coord)
 
     def __str__(self):
         return f"{type(self).__name__}-{self.uid}"
@@ -52,7 +54,7 @@ class Cell():
 
     @classmethod
     def load_cells(cls,cells_path):
-        cls.cell_parsers[pathlib.Path(cells_path).suffix](cells_path)
+        return cls.cell_parsers[pathlib.Path(cells_path).suffix](cells_path)
 
     @staticmethod
     def create_auto_cells(cell_dict):
@@ -61,48 +63,65 @@ class Cell():
         if not all (key in cell_dict for key in auto_cells_required_keys):
             raise ValueError(f"Missing one of required keys {auto_cells_required_keys}")
 
-        coord_system = CoordinateSystem.get_coord_system(cell_dict["coord_type"])
+        coord_system = CoordinateSystemMgr.get_coord_system(cell_dict["system"])
 
-        coords = coord_system.get_coord_set(dimensions)
+        coords = coord_system.get_coord_set(cell_dict['dimensions'])
 
         for idx,coord in enumerate(coords):
-            cells.add(Cell(cell_type=cell_dict["cell_type"],coord=coord, attributes=cell_dict["attributes"]))
+            cells.add(Cell(coord=coord,**cell_dict))
+
+        return cells
 
 
     @classmethod
     def create_cell(cls,cell_dict):
+        cells = list()
         #Passthrough all the initialization we can for simple construction
-        cell=Cell(**cell_dict)
+        cell = Cell(**cell_dict)
 
         #auxiliary property parsing
-        cell.attributes.update(cell_dict.attributes)
+        if 'attributes' in cell_dict:
+            cell.attributes.update(cell_dict['attributes'])
+
+        cells.append(cell)
+        cells.extend(cls.create_subcells(cell_dict))
+
+        return cells
+
+    @classmethod
+    def create_subcells(cls,cell_dict):
+        cells = list()
 
         if "cell" in cell_dict:
-            create_cell(cell_dict["cell"])
+            cells.extend(cls.create_cell(cell_dict["cell"]))
 
         if "cells" in cell_dict:
             for cell in cell_dict["cells"]:
                 #parse subcells
-                create_cell(cell)
+                cells.extend(cls.create_cell(cell))
 
         if "auto_cells" in cell_dict:
             #Instantiate the auto_cells
-            create_auto_cells(cell_dict["auto_cells"])
+            cells.extend(cls.create_auto_cells(cell_dict["auto_cells"]))
 
-
+        return cells
 
 @Cell.register_cell_parser('.json')
 def load_cell_json(cell_path):
     import json
     file_path = pathlib.Path(cell_path)
-    logging.debug(F"File path: {file_path.absolute()}")
+    #logging.debug(F"File path: {file_path.absolute()}")
     with open(cell_path, "r") as json_cells:
         data = json.load(json_cells)
+
+    return Cell.create_subcells(data)
 
 @Cell.register_cell_parser('.yml')
 def load_cell_yml(cell_path):
     import yaml
     file_path = pathlib.Path(cell_path)
-    logging.debug(F"File path: {file_path.absolute()}")
+    #logging.debug(F"File path: {file_path.absolute()}")
     with open(cell_path, "r") as yaml_cells:
         data = yaml.load(yaml_cells)
+
+    return Cell.create_subcells(data)
--- src/boardgame_framework/utils.py
+++ src/boardgame_framework/utils.py
@@ -1,8 +1,18 @@
+import logging
 import pathlib
 from . import cell
 from enum import Enum
 from dataclasses import dataclass
 
+logger = logging.getLogger(__name__)
+
+def checkallequal(iteratable):
+    iterator = iter(iteratable)
+    try:
+        first = next(iterator)
+    except StopIteration:
+        return True
+    return all(first == rest for rest in iterator)
 
 class CoordinateSystemMgr():
     coord_systems = dict()
@@ -14,21 +24,15 @@ class CoordinateSystemMgr():
         def anon_reg_func(coord_system):
             if not issubclass(coord_system,CoordinateSystem):
                 raise ValueError(F"Passed class {coord_system.__name__} not of type CoordinateSystem")
-            coord_system.shortname = system_name
 
             #register both the short name and the full class name
-            cls.coord_systems[system_name] = coord_system
-            cls.coord_systems[coord_system.__name__] = coord_system
+            instance = coord_system(system_name = system_name)
+            cls.coord_systems[system_name] = instance
             return coord_system
         return anon_reg_func
 
     @classmethod
     def get_coord_system(cls,system_name):
-        if system_name in cls.coord_systems:
-            return cls.coord_systems[system_name]()
-
-    @classmethod
-    def get_coord_type(cls,system_name):
         if system_name in cls.coord_systems:
             return cls.coord_systems[system_name]
 
@@ -42,6 +46,14 @@ class CoordinateSystem():
 
     def __repr__(self):
         return F"{type(self).__name__}-{id(self)}"
+
+    def __init__(self, system_name):
+        if not system_name:
+            raise ValueError(f"System missing name parameter")
+        self.system_name=system_name
+
+    def __hash__(self):
+        return hash(self.system_name)
 
     @classmethod
     def register_to_converter(cls,coord_system):
@@ -57,96 +69,201 @@ class CoordinateSystem():
             return callback
         return anon_reg_func
 
-    @classmethod
-    def to_other_system(cls,name,coord):
-        if name in to_other_conversions:
-            to_other_conversions[name](coord)
+    def to_other_system(self,name,coord):
+        #logger.debug("%s: from_other_conversions: %s",type(self).__name__,self.from_other_conversions)
+        if name in self.to_other_conversions:
+            return self.to_other_conversions[name](coord)
         else:
-            raise ValueError(F"No Converter to {name} Found")
+            raise ValueError(F"No Converter from {type(self).__name__} to {name} Found")
 
 
-    @classmethod
-    def from_other_system(cls,name,coord):
-        if name in to_other_conversions:
-            to_other_conversions[name](coord)
+    def from_other_system(self,name,coord):
+        #logger.debug("%s: from_other_conversions: %s",type(self).__name__, self.from_other_conversions)
+        if name in self.from_other_conversions:
+            return self.from_other_conversions[name](self,coord)
         else:
-            raise ValueError(F"No Converter from {name} Found")
+            raise ValueError(F"No Converter from {name} to {type(self).__name__} Found")
 
-
-@CoordinateSystemMgr.register_coord_system('cubed')
-class CubedCoordinateSystem(CoordinateSystem):
-    dimension_req_keys=['x','y','z']
-
-
-
-    #coord_type = cubed_coord
-
-    class FromRectOffsetType(Enum):
-        ODDR = 1
-        EVENR = 2
-        ODDC = 3
-        EVENC = 4
-
-    def __init__(self):
-        pass
-
-
-
-    def coord():
-        return cubed_coord(**kwargs)
-
-    def get_coord_set(self,dimensions):
-        #safety checking
-        if not all (key in dimensions for key in self.dimension_req_keys):
-            raise ValueError(f"Missing one of required keys {self.dimension_req_keys}")
-
-    def validate_dimensions(self,dimensions):
-        #safety checking
-        if not all (key in dimensions for key in self.dimension_req_keys):
-            raise ValueError(f"Missing one of required keys {self.dimension_req_keys}")
-
-        if not all(isinstance(elem) for elem in dimensions):
-            raise ValueError(f"Not all dimensions are ints {dimensions}")
-
-    def get_coord_set(self,dimensions):
-        assert False, "Not Implemented Yet"
-
-        return coords
-
-    def from_rect_coord(self,rect_coord,offset_type=FromRectOffsetType.ODDR):
-        assert(rect_coord.coord_type==RectCoordinateSystem)
-
-    @CoordinateSystem.register_from_converter("rect")
-    @staticmethod
-    def from_oddr(coord):
-        x = coord.x - (coord.y - (coord.y&1)) / 2
-        z = coord.y
-        y = -x-z
-        return cubed_coord(x, y, z)
-
-    @CoordinateSystem.register_to_converter("rect")
-    @staticmethod
-    def to_oddr(coord):
-        col = coord.x + (coord.z - (coord.z&1)) / 2
-        row = coord.z
-        return RectCoordinateSystem.rect_coord(col, row)
-
-
-@dataclass
+@dataclass(eq=True,frozen=True)
 class cubed_coord():
     x: int
     y: int
     z: int
-    system: CoordinateSystem = CubedCoordinateSystem
+    system: CoordinateSystem
+
+    def __add__(self,coord):
+        assert type(self) == type(coord)
+        assert self.system == coord.system, "The coordinates must belong to the same system"
+        return cubed_coord(x=self.x+coord.x,y=self.y+coord.y,z=self.z+coord.z,system=self.system)
+
+    def __sub__(self,coord):
+        assert type(self) == type(coord)
+        assert self.system == coord.system, "The coordinates must belong to the same system"
+        return cubed_coord(x=self.x-coord.x,y=self.y-coord.y,z=self.z-coord.z,system=self.system)
+
+    def __mul__(self,multiplier):
+        assert isinstance(multiplier, int)
+        return cubed_coord(x=self.x*multiplier,y=self.y*multipliers,z=self.z*multiplier,system=self.system)
+
+@dataclass(eq=True,frozen=True)
+class hex_coord(cubed_coord):
+    def __post_init__(self):
+        assert x+y+z == 0
+
+
+@dataclass(eq=True,frozen=True)
+class rect_coord():
+    x: int
+    y: int
+    system: CoordinateSystem
+
+    def __add__(self,coord):
+        assert type(self) == type(coord)
+        assert self.system == coord.system, "The coordinates must belong to the same system"
+        return rect_coord(x=self.x+coord.x,y=self.y+coord.y,system=self.system)
+
+    def __sub__(self,coord):
+        assert type(self) == type(coord)
+        assert self.system == coord.system, "The coordinates must belong to the same system"
+        return rect_coord(x=self.x-coord.x,y=self.y-coord.y,system=self.system)
+
+    def __mul__(self,multiplier):
+        assert isinstance(multiplier, int)
+        return rect_coord(x=self.x*multiplier,y=self.y*multipliers,system=self.system)
+
+
+@CoordinateSystemMgr.register_coord_system('hex')
+class HexCoordinateSystem(CoordinateSystem):
+    dimension_req_keys=['x','y','z']
+
+    def __init__(self,**kwargs):
+        print("%s: __init__ %s",type(self).__name__,kwargs)
+        super().__init__(**kwargs)
+        self.dirs=[cubed_coord(1,-1,0,self),cubed_coord(+1,0,-1,self),cubed_coord(0,1,-1,self),
+                   cubed_coord(-1,1,0,self),cubed_coord(-1,0,1,self),cubed_coord(0,-1,1,self)]
+
+    def coord(self,**kwargs):
+        return cubed_coord(system=self,**kwargs)
+
+    def distance(self,coord1,coord2):
+        return (abs(coord1.x - coord2.x) + abs(coord1.y - coord2.y) + abs(coord1.z - coord2.z))//2
+
+    def coords_in_range(self,coord1,steps):
+        coords = list()
+        xl = -steps
+        xh = steps
+
+        #Generate using an axial formula to make it easier
+        #calculate z via the other two and throw away ones that aren't in bounds
+        for x in range(xl,xh+1):
+            for y in range(max(-steps,-x-steps),min(steps,-x+steps)+1):
+                z=-x-y
+                coords.append(coord1+cubed_coord(x,y,z,system=self))
+        return coords
+
+    def get_coord_set(self,dimensions):
+        #safety checking
+        if not all (key in dimensions for key in self.dimension_req_keys):
+            raise ValueError(f"Missing one of required keys {self.dimension_req_keys}")
+
+        assert False, "Not Implemented Yet"
+
+    def validate_dimensions(self,dimensions):
+
+        #safety checking
+        if not all (key in dimensions for key in self.dimension_req_keys):
+            raise ValueError(f"Missing` one of required keys {self.dimension_req_keys}")
+
+        if not all(isinstance(elem,int) for elem in dimensions.values()):
+            raise ValueError(F"Not all dimensions are ints {dimensions}")
+
+        if not all(elem > 0 for elem in dimensions.values()):
+            raise ValueError(F"Dimensions must be greater than 1 {dimensions}")
+
+        if not all(elem > 0 for elem in dimensions.values()):
+            raise ValueError(F"Dimensions must be greater than 1 {dimensions}")
+
+        if not checkallequal(dimensions.values()):
+            raise ValueError(F"Not all dimensions are equal {dimensions}.  They must be equal.")
+
+    def get_coord_set(self,dimensions):
+
+        logger.debug("%s(%s): get_coord_set: dimensions: %s",self.system_name,type(self).__name__,dimensions)
+        self.validate_dimensions(dimensions)
+
+        coords = self.coords_in_range(cubed_coord(0,0,0,system=self),dimensions['x']//2)
+
+        logger.debug("%s(%s):%s",self.system_name,type(self).__name__,str(coords))
+        logger.debug("%s(%s): get_coord_set: dimensions: %s numcells: %s",self.system_name,type(self).__name__,dimensions,str(len(coords)))
+        return coords
+
+
+@HexCoordinateSystem.register_from_converter("oddr")
+def cube_from_oddr(newsystem,coord):
+    x = coord.x - (coord.y - (coord.y&1)) // 2
+    z = coord.y
+    y = -x-z
+    return newsystem.coord(x=x, y=y, z=z)
+
+@HexCoordinateSystem.register_to_converter("oddr")
+def cube_to_oddr(newsystem,coord):
+    y = coord.x + (coord.z - (coord.z&1)) // 2
+    x = coord.z
+    return newsystem.coord(x=x,y=y)
+
+@HexCoordinateSystem.register_from_converter("evenr")
+def cube_from_evenr(newsystem,coord):
+    x = coord.x - (coord.y + (coord.y&1)) // 2
+    z = coord.y
+    y = -x-z
+    return newsystem.coord(x=x, y=y, z=z)
+
+@HexCoordinateSystem.register_to_converter("evenr")
+def cube_to_evenr(newsystem,coord):
+    y = cube.x + (cube.z + (cube.z&1)) // 2
+    x = coord.z
+    return newsystem.coord(x=x,y=y)
+
+@HexCoordinateSystem.register_to_converter("oddq")
+def cube_to_oddq(newsystem,coord):
+    x = coord.x
+    y = coord.z + (coord.x - (coord.x&1)) // 2
+    return newsystem.coord(x=x,y=y)
+
+@HexCoordinateSystem.register_to_converter("oddq")
+def oddq_to_cube(newsystemm,coord):
+    x = coord.x
+    z = coord.y - (coord.x - (coord.x&1)) // 2
+    y = -x-z
+    return newsystem.coord(x=x,y=y,z=z)
+
+@HexCoordinateSystem.register_to_converter("evenq")
+def cube_to_evenq(newsystemm,coord):
+    col = coord.x
+    row = coord.z + (coord.x + (coord.x&1)) // 2
+    return newsystem.coord(x=x,y=y)
+
+@HexCoordinateSystem.register_to_converter("evenq")
+def evenq_to_cube(newsystemm,coord):
+    x = coord.x
+    z = coord.y - (coord.x + (coord.x&1)) // 2
+    y = -x-z
+    return newsystem.coord(x=x,y=y,z=z)
 
 @CoordinateSystemMgr.register_coord_system('rect')
+@CoordinateSystemMgr.register_coord_system('square')
+@CoordinateSystemMgr.register_coord_system('oddr')
+@CoordinateSystemMgr.register_coord_system('oddq')
+@CoordinateSystemMgr.register_coord_system('evenr')
+@CoordinateSystemMgr.register_coord_system('evenq')
 class RectCoordinateSystem(CoordinateSystem):
     dimension_req_keys=['x','y']
 
-
-
-    def __init__(self):
-        pass
+    def __init__(self,**kwargs):
+        logger.debug("%s: __init__ %s",type(self).__name__,kwargs)
+        super().__init__(**kwargs)
+        self.dirs=[rect_coord(-1,0,self),rect_coord(0,-1,self),rect_coord(1,0,self),
+                   rect_coord(0,1,self)]
 
     def __str__(self):
         return F"{type(self).__name__}"
@@ -154,27 +271,27 @@ class RectCoordinateSystem(CoordinateSystem):
     def __repr__(self):
         return F"{type(self).__name__}-{id(self)}"
 
+    def coord(self,**kwargs):
+        return cubed_coord(system=self,**kwargs)
+
     def validate_dimensions(self,dimensions):
         #safety checking
         if not all (key in dimensions for key in self.dimension_req_keys):
             raise ValueError(F"Missing one of required keys {self.dimension_req_keys}")
 
-        if not all(isinstance(elem) for elem in dimensions):
+        if not all(isinstance(elem,int) for elem in dimensions.values()):
             raise ValueError(F"Not all dimensions are ints {dimensions}")
 
     def get_coord_set(self,dimensions):
         coords = set()
 
-        validate_dimensions(dimensions)
+        logger.debug("%s(%s): get_coord_set: dimensions: %s",self.system_name,type(self).__name__,dimensions)
+        self.validate_dimensions(dimensions)
 
         for x in range(0,dimensions['x']):
             for y in range(0,dimensions['y']):
-                coords.add(rect_coord(x,y))
+                coords.add(rect_coord(x,y,system=self))
+
+        logger.debug("%s(%s): get_coord_set: dimensions: %s numcells: %s",self.system_name,type(self).__name__,dimensions,str(len(coords)))
 
         return coords
-
-@dataclass
-class rect_coord():
-    x: int
-    y: int
-    coord_type: CoordinateSystem = RectCoordinateSystem
