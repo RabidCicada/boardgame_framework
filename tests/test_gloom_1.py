9ff902a69cd71318770f412abc2a64f432b2b927
Kyle Stapp <kstapp@redlattice.com>
2019-02-07 17:57:14 -0500

    Abstracting path so it'll run on any system

--- tests/test_gloom_1.py
+++ tests/test_gloom_1.py
@@ -1,8 +1,9 @@
 from boardgame_framework.cell import CellMgr
 import boardgame_framework.utils as utils
+import pathlib
 
 def test_multiroom():
-    cells = CellMgr.load_cells("gloomhaven_scenario1.yml",basedir="../tests/data_files/")
+    cells = CellMgr.load_cells("gloomhaven_scenario1.yml",basedir=pathlib.Path('..','tests','data_files'))
 
     #5 top level cells + 35 + 24 + 30
     assert len(cells) == 94
