75dc36f3684feb281ba90e67a4560af7cb7a7a6b
dev <dev@localhost.localdomain>
2022-04-05 08:43:53 -0400

    Initial attempt at replumb of build tools etc

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
 #!/usr/bin/env python
-from setuptools import setup
-
-setup(
-    setup_requires=['pbr',"pytest-runner"],
-    package_dir={'':'src/'}, # Needed for distutils
-    tests_require=["pytest"],
-    pbr=True
-)
-
+import setuptools
+setuptools.setup()
